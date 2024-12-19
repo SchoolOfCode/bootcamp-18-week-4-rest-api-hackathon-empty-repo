38ea75aa3f72e4f97b38b2e6f29a23d26d7cc2b7
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2024-12-19 12:21:05 +0000

    Started psuedocode for init setup and first route

---
+++ app.js
@@ -0,0 +1,21 @@
+/* --- project initialise ---*/
+// npm init (-y) and then npm i express 
+// change package.json "type":"module" above "scripts"
+// create bin folder -> file called www.js
+// package.json "start": "node bin/www.js" (storing the PORT ie 8080)
+// package.json "dev": "node --watch bin/www.js"  -> npm run dev
+// import the dataset
+
+/* --- imports ---*/
+// import express from express
+
+
+
+// Set Router as a variable from the express module (using sub routing)
+
+/* --- routes ---*/
+
+// GET all weapons
+// To listen for a "/" and calling an async function with a request and response
+// after receiving the request we get all the weapons and respond with a json format
+
